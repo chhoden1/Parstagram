f38eedf7356d015899c62b764db03f340c751398
chhodensherpa <76025934+chhodensherpa@users.noreply.github.com>
2021-03-11 11:44:21 -0800

    Worked on SignUp View Controller code

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "5XF1ddrclb24gbjywtCQrK6tI5Cs9irUSIJhXxEx"
+            $0.clientKey = "qPtPXzUq1IB1bhZV6aweBz9RrwWJtn66nPZjdmEI"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
