71aa1ae811e6448afd0e115f56b7ecac23e5221a
chhodensherpa <76025934+chhodensherpa@users.noreply.github.com>
2021-03-15 00:02:48 -0700

    Worked on Displaying comments

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -70,10 +70,10 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
             
             let comment = comments[indexPath.row - 1]
-            cell.nameLabel.text = comment["text"] as? String
+            cell.commentLabel.text = comment["text"] as? String
             
             let user = comment["author"] as! PFUser
-            cell.commentLabel.text = user.username
+            cell.nameLabel.text = user.username
             return cell
         }
         
